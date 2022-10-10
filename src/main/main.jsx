d358466d63bbbe464f1c39321c32bfecc7119b18
cccamerini@yahoo.com <cccamerini@yahoo.com>
2022-10-10 06:59:44 -0300

    Merge branch 'master' of https://github.com/cccamerini/pragmata

---
+++ src/main/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Trans } from 'react-i18next';
+import '../bootstrap.min.css';
+import '../App.css';
+export default function Main() {
+  return (
+    <Trans i18nKey="intro">
+      
+    </Trans>
+  )
+}
